ac66385f24e721ec273a8124bb05ea117157ab0c
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-09-30 21:13:54 +0530

    Variables in JavaScript

---
+++ variables/index.js
@@ -0,0 +1,45 @@
+// variable = A container that stores a value.
+//            Behaves as if it were the value it contains.
+
+// 1. declaration    let x;
+// 2. assignment      x = 100;
+
+
+// NUMBERS
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// console.log(`You are ${age} years old`);
+// console.log(`The price is $${price}`);
+// console.log(`Your gpa is: ${gpa}`);
+
+
+// STRINGS
+
+// let firstName= "Ankit"
+// let favouriteFood = " pizza";
+// let email = "Learning123@gmail.com"
+
+
+// BOOLEAN
+
+// let online = false;
+// let forSale = true;
+// let isStudent = true;
+
+// console.log(`Ankit is online: ${online}`);
+// console.log(`Is this car for same: ${forSale}`);
+// console.log(`Enrolled: ${isStudent}`);
+
+let fullName = "Ankit Singh";
+let age = 50;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+
+
