e7215d7af2ae81eb2ad6d9f2126cdd0207f7b231
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-09-30 21:53:06 +0530

    Arithmetic Operators

---
+++ Arithmetic Operators/index.js
@@ -0,0 +1,40 @@
+// arithmetic operators = operands (values, variables. etc.)
+//                        operators (+ - * /)
+//                        ex. 11 = x + 5;
+
+// let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 3;
+// let extraStudents = students % 3;
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students++;
+// students--;
+
+/* 
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+let result =  6/2 ** (2 + 5);
+
+console.log(result);
+
+
+
+
+
+
