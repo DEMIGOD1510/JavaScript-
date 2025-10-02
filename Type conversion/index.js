16e11fa8a6150991b1badd268060d432565dcf26
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-10-02 16:53:02 +0530

    Type Conversion

---
+++ Type conversion/index.js
@@ -0,0 +1,53 @@
+// type conversion = change the datatype of a value to another 
+//                    (strings, numbers, booleans)
+
+// let age = window.prompt("How old are you?");
+// age = Number(age);
+// age+=1;
+
+// console.log(age, typeof age);
+
+
+// IF THE VALUE IS STRING
+
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x); 
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+
+// IF THE VALUE IS NUMBER
+// let x = "0";
+// let y = "0";
+// let z = "0";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x); 
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+
+// IF THE VERABLE HAS NO VALUE
+let x;
+let y;
+let z;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x); 
+console.log(y, typeof y);
+console.log(z, typeof z);
