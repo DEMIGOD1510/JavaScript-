8a6741605b4095fb7af57a9416234ddc8435082b
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-10-03 14:38:43 +0530

    Math in JavaScript

---
+++ Math object/index.js
@@ -0,0 +1,89 @@
+// Math = built-in object that provides a 
+//        collection of properties and methods 
+
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+
+// TO ROUND A NUMBER
+// let x = 3.21;
+// let y = 2;
+// let z;
+
+// z = Math.round(x);
+
+// console.log(z);
+
+
+//TO FLOOR A NUMBER
+// let x = 3.99;
+// let y = 2;
+// let z;
+
+// z = Math.floor(x);
+
+// console.log(z);
+
+
+//TO ALWAYS ROUND UP YOU CAN USE CEIL
+// let x = 3.21;
+// let y = 2;
+// let z;
+
+// z = Math.ceil(x);
+
+// console.log(z);
+
+
+//USE (TRUNC) TO ELIMINATE ANY DECIMAL
+// let x = 3.21;
+// let y = 2;
+// let z;
+
+// z = Math.trunc(x);
+
+// console.log(z);
+
+
+// USE (POW) TO RAISE TO A GIVEN POWER
+// let x = 3;
+// let y = 2;
+// let z;
+
+// z = Math.pow(x, y);
+
+// console.log(z);
+
+
+//USE (SQRT) FOR SQUARE ROOT
+// let x = -3.21;
+// let y = 2;
+// let z;
+
+ //z = Math.sqrt(x);
+
+// console.log(z);
+
+
+
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+//z = Math.log(x);
+//z = Math.sin(x);
+//z = Math.cos(x);
+//z = Math.tan(x);
+//z = Math.abs(x);
+//z = Math.sign(x);
+//z = Math.sign(x);
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(min);
+
+
+
+
