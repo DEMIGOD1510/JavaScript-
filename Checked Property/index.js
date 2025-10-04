b66f3da8e165cf9435a7dd04e6dc4c2291a035f5
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-10-04 21:12:15 +0530

    checkbox in javascript

---
+++ Checked Property/index.js
@@ -0,0 +1,36 @@
+// .checked = property that determines the checked state of an 
+//            HTML checked or radio button element
+
+const myCheckbox = document.getElementById("myCheckbox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");    
+const payPalBtn = document.getElementById("payPalBtn");   
+const mySubmit = document.getElementById("mySubmit");   
+const subResult = document.getElementById("subResult");   
+const paymentResult = document.getElementById("paymentResult");   
+
+mySubmit.onclick = function () {
+
+  if(myCheckbox.checked) {
+    subResult.textContent = `You are subscribed`;
+  }
+  else {
+    subResult.textContent = `You are NOT subscribed!`;
+  }
+
+  if(visaBtn.checked) {
+    paymentResult.textContent = `You are paying with visa`;
+  }
+  else if (masterCardBtn.checked) {
+    paymentResult.textContent = `You are paying with MasterCard`;
+  }
+  else if (payPalBtn.checked) {
+    paymentResult.textContent = `You are paying with PayPal`;
+  }
+  else {
+    paymentResult.textContent = `You must select a paymest type`;
+  }
+}
+
+
+
