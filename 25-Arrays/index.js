cca3e7dc57f643f6482b012b71b7e17d2f555f34
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-10-05 21:14:50 +0530

    Arrays in JavaScritp

---
+++ 25-Arrays/index.js
@@ -0,0 +1,24 @@
+// array = a variable like structure that can hold
+//         more than 1 value
+
+
+let fruits = ["mango", "orange", "banana", "apple", "coconut"];
+
+// fruits.push("apple");
+// fruits.pop();
+// fruits.unshift("coconut");
+// fruits.shift();
+
+
+// let numOfFruits = fruits.length;
+// let index = fruits.indexOf("apple");
+
+// console.log(numOfFruits);
+// console.log(index);
+
+for(let i = fruits.length - 1; i >= 0; i--){
+  console.log(fruits[i]);
+}
+
+
+
