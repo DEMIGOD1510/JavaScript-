352b2b6fd785351373d855c64bf3a2ebda19cebd
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-10-05 11:30:08 +0530

    String Method in JavaScript

---
+++ String Methods/index.js
@@ -0,0 +1,61 @@
+// string methods = allow you to manipulate and work with text (strings)
+
+
+// let userName = "AnkitSingh";
+
+
+// console.log(userName.charAt(0));
+
+// console.log(userName.lastIndexOf("i"));
+
+// console.log(userName.length);
+
+// userName = userName.trim();
+
+// userName = userName.toUpperCase();
+
+// userName = userName.toLowerCase();
+
+// userName = userName.repeat(3);
+
+// let result = userName.startsWith(" ");
+
+// if(result) {
+//   console.log("Your userName can't begin with ' '")
+// }
+// else{
+//   console.log(userName)
+// }
+
+// let result = userName.endsWith(" ");
+
+// if(result) {
+//   console.log("Your userName can't ends with ' '");
+// }
+// else{
+//   console.log(userName)
+// }
+
+// let result = userName.includes(" ");
+
+// if(result) {
+//   console.log("Your userName can't include ' '");
+// }
+// else{
+//   console.log(userName)
+// }
+
+// let phoneNumber = "123-456-7890";
+
+// phoneNumber = phoneNumber.replaceAll("-", "/");
+
+// console.log(phoneNumber);
+
+
+let phoneNumber = "123-456-7890";
+
+phoneNumber = phoneNumber.padEnd(15, "0");
+
+console.log(phoneNumber);
+
+
