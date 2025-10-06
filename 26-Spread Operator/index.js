58e669cef7721595d5ab3450fe96740f260a18ce
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-10-06 21:43:19 +0530

    Spread Operator in JavaScript

---
+++ 26-Spread Operator/index.js
@@ -0,0 +1,25 @@
+// Spread Operator = ... allows an iterable such as an array or
+//                    string to be expended into seperate elements 
+//                    (unpacks the elements)
+
+// let numbers = [1, 2, 3, 4, 5];
+// let maximum = Math.max(...numbers);
+// let minimum = Math.min(...numbers);
+
+// console.log(minimum);
+
+// let username = "Ankit Singh";
+// let letters = [...username].join("-");
+
+// console.log(letters);
+
+let fruits = ["apple", "mango", "banana"];
+let vegetables = ["carrots", "potatos", "tomatos"];
+
+let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+console.log(newFruits);
+
+
+
+
