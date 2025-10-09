b0686b02e2747c3a0d46aed9ef8262dc4d667e4d
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-10-09 21:41:11 +0530

    Function expressions

---
+++ 35-Function experssions/index.js
@@ -0,0 +1,50 @@
+// function decleration = define a reusable block of code that performs a specific task
+
+// function hello(){
+//   console.log("Hello")
+// }
+
+// function expression = a way to define function as values or variables
+
+// const hello = function(){
+//   console.log("Hello");
+// }
+
+// hello();
+
+// setTimeout(function(){
+//   console.log("Hello");
+// }, 3000);
+
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const squares = numbers.map(function(element){
+  return Math.pow(element, 2);
+});
+
+const cubes = numbers.map(function(element){
+  return Math.pow(element, 3);
+});
+
+const evenNums = numbers.filter(function(element){
+  return element % 2 === 0;
+});
+
+const oddNums = numbers.filter(function(element){
+  return element % 2 !== 0;
+});
+const total = numbers.reduce(function(accumulator, element){
+  return accumulator + element;
+});
+
+console.log(total);
+
+
+
+
+
+
+
+
+
+
