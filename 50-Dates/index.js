b5b42b51b05940569416d2a15b39a94b3f8b32c5
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-10-13 22:12:04 +0530

    Dates in JavaScript

---
+++ 50-Dates/index.js
@@ -0,0 +1,38 @@
+// Data objects = Objects that contain values that represent dates and times
+//                These data objexts can be changed and formatted 
+
+// const date = new Date();
+
+// date.setFullYear(2026);
+// date.setMonth(0);
+// date.setDate(1);
+// date.setMinutes(3);
+// date.setSeconds(4);
+
+// console.log(date);
+
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDate();
+// const hour = date.getHours();
+// const minutes = date.getMinutes();
+// const seconds = date.getSeconds();
+// const dayOfWeek = date.getDay();
+
+
+// console.log(year);
+// console.log(month);
+// console.log(day);
+// console.log(hour);
+// console.log(minutes);
+// console.log(seconds);
+// console.log(dayOfWeek);
+
+
+const date1 = new Date("2025-12-31");
+const date2 = new Date("2026-01-01");
+
+if(date2 > date1){
+  console.log("HAPPY NEW YEAR!")
+}
+
