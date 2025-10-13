5c8acbfd06a24f46bc0acea537e9175459e3b654
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-10-13 22:12:58 +0530

    setTimeout in JavaScript

---
+++ 52-setTimeout()/index.js
@@ -0,0 +1,34 @@
+// setTimeout() = function in JavaScript that allows you to schedule
+//                the execution of a function after an amount of time (milliseconds)
+//                Times are approximate (varies bsed on the workload of the           
+//                Javascript runtime env.)
+//                 setTimeout(callback, delay);
+
+
+
+// function sayHello(){
+//   window.alert("Hello");
+// }
+
+// setTimeout(sayHello, 3000);
+
+
+// setTimeout(function(){window.alert("Hello")}, 3000);
+
+// const timeoutId = setTimeout(() => window.alert("Hello"), 3000);
+
+// clearTimeout(timeoutId);
+
+let timeoutId;
+
+function startTimer(){
+  timeoutId = setTimeout(() => window.alert("Hello"), 3000);
+  console.log("STARTED");
+}
+
+function clearTimer(){
+  clearTimeout(timeoutId);
+  console.log("CLEARED")
+}
+
+
