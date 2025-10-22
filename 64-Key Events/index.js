ef17480788f3854e52c349cb8cd3eacc7cdfb605
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-10-22 21:03:07 +0530

    Key events in JavaScript

---
+++ 64-Key Events/index.js
@@ -0,0 +1,54 @@
+// eventListener = Listen for specific events to create interactive web pages
+//                 events: keydoun, keyup
+//                 .addEventListener(event, callback);
+
+
+const myBox = document.getElementById("myBox");
+const moveAmount = 10;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", event => {
+
+  if(event.key.startsWith("Arrow")){
+
+    event.preventDefault();
+
+    switch(event.key){
+      case "ArrowUp":
+        y -= moveAmount;
+        break;
+      case "ArrowDown":
+        y += moveAmount;
+        break;
+      case "ArrowLeft":
+        x -= moveAmount;
+        break;
+      case "ArrowRight":
+        x += moveAmount;
+        break;
+    }
+    myBox.style.top = `${y}px`;
+    myBox.style.left = `${x}px`;
+  }
+})
+
+document.addEventListener("keydown", event => {
+  myBox.textContent = "😍";
+  myBox.style.backgroundColor = "lightblue";
+});
+
+document.addEventListener("keyup", event => {
+  myBox.textContent = "❤️";
+  myBox.style.backgroundColor = "pink"
+});
+
+
+
+
+
+
+
+
+
+
